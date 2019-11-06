0442fb34173150c3d9c4b0930a969a6411ec61b0
Michael Patel <michael.patel94@gmail.com>
2019-11-06 18:57:21 -0500

    Adding help()

---
+++ Python/hello_python.py
@@ -0,0 +1,12 @@
+"""
+Michael Patel
+November 2019
+
+https://www.kaggle.com/colinmorris/hello-python
+
+"""
+#####################################################
+
+
+#####################################################
+help(round)
