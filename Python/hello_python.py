ef2d07d740342ea14deccc83ace8613cffc4dbae
Michael Patel <michael.patel94@gmail.com>
2019-11-06 19:03:02 -0500

    Adding docstring to defn

--- Python/hello_python.py
+++ Python/hello_python.py
@@ -9,4 +9,20 @@ https://www.kaggle.com/colinmorris/hello-python
 
 
 #####################################################
-help(round)
+def least_difference(a, b, c):
+    """
+    :param a:
+    :param b:
+    :param c:
+    :return: the smallest difference between any two numbers among a, b, c
+    """
+
+    diff1 = abs(a-b)
+    diff2 = abs(b-c)
+    diff3 = abs(a-c)
+
+    return min(diff1, diff2, diff3)
+
+
+# define docstring first, so that help() will display docstring
+help(least_difference)
